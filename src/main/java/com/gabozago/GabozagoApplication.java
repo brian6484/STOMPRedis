7981bb7489e75806223b6c33cdcc363ab718254e
choidongkuen <danaver12@daum.net>
2023-08-20 22:13:48 +0900

    initial commit

--- src/main/java/com/gabozago/GabozagoApplication.java
+++ src/main/java/com/gabozago/GabozagoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GabozagoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GabozagoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GabozagoApplication.class, args);
+	}
 
 }
