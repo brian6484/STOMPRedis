03530bcd67c71bad466663f30bc2f0e3cbd26840
choidongkuen <danaver12@daum.net>
2023-08-20 22:12:31 +0900

    initial commit

---
+++ src/main/java/com/gabozago/GabozagoApplication.java
@@ -0,0 +1,13 @@
+package com.gabozago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabozagoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GabozagoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabozago/GabozagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabozago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabozagoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
